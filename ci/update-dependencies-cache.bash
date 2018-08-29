ef8f7ceebbe6daf80b0fa471aac9868f88df5cae
Millian Poquet <millian.poquet@gmail.com>
2018-08-29 23:41:34 +0200

    [ci] update dependencies cache (cachix)

---
+++ ci/update-dependencies-cache.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p nix
+set -eu
+
+# (re)build up-to-date CI batsched package + deps, push them on binary cache
+nix-build ./ci -A test_stable | cachix push batsim
+nix-build ./ci -A test_dev | cachix push batsim
