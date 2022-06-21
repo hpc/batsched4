8050e30170d904ef24ef2db4094dc8a637cf9d18
Craig Walker <cswalke1@coastal.edu>
2022-06-21 11:01:30 -0400

    initial commit, changed svg a little,min width=120

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -1190,6 +1190,9 @@ void Schedule::output_to_svg(const string &filename_prefix)
     */
    LOG_F(INFO,"%s",to_string().c_str());
     write_svg_to_file(buf);
+    Rational end = (_profile.end()--)->end;
+    if (end != 1e19)
+        _previous_time_end = end;
 
 
     delete[] buf;
