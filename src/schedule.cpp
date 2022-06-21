d4f2c75b2078b96f11eb1b61fa62cb596d177d03
Craig Walker <cswalke1@coastal.edu>
2022-06-21 09:56:34 -0400

    initial commit, changed svg a little,min width=120

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -1043,11 +1043,11 @@ string Schedule::to_svg() const
     Rational sim_time = _profile.begin()->begin;
     if(_profile.size() == 1)
         sim_time=_previous_time_end;
-    TimeSliceIterator slice_it = _profile.end();
-    TimeSlice end_slice = *(slice_it--);
+    TimeSliceIterator slice_it = _profile.end()--;
+    Rational myEnd = slice_it->end;
 
-    if(end_slice.end !=1e19)
-        _previous_time_end = end_slice.end;
+    if(myEnd !=1e19)
+        _previous_time_end = myEnd;
     snprintf(buf, buf_size,
         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
         "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%g\" height=\"%g\">\n"
