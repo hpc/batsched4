b4f7d1b37acc7bb54eb3fd9e0c29b0ef2e38a60c
Craig Walker <cswalke1@coastal.edu>
2023-04-26 08:23:58 -0400

    fixed filesystem

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -10,7 +10,7 @@
 #include "pempek_assert.hpp"
 #if __has_include(<filesystem>)
 #include <filesystem>
-namespace fs = std::fileystem;
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
